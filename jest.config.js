7d83247fb4e905e3af8760cb389ab2f9f5b4ec15
manuelsalgado <salgadopietrini7@gmail.com>
2022-07-11 15:09:04 +0100

    configure test coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  verbose: true,
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["dist"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts, tsx, js, jsx}", "!src/**/*.d.ts"],
+  coverageReporters: ["text", "html"],
 };
